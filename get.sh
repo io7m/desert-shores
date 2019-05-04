fedf7b62190d9a6932963ff967fb3cd8f7918ce3
Mark Raynsford <code@io7m.com>
2019-05-04 15:26:05 +0100

    Add Thermal Expansion and checksum verification

--- get.sh
+++ get.sh
@@ -7,11 +7,50 @@ wget \
   -O jar-base/forge.jar \
   https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.2-14.23.5.2836/forge-1.12.2-14.23.5.2836-universal.jar
 
+#------------------------------------------------------------------------
+# Just Enough Items
+
 wget \
   -O jar/jei_1.12.2-4.15.0.268.jar \
   https://minecraft.curseforge.com/projects/jei/files/2682936/download
+
+#------------------------------------------------------------------------
+# Xaero's Minimap
 
 wget \
   -O jar/Xaeros_Minimap_1.16.6_Forge_1.12.jar \
   https://minecraft.curseforge.com/projects/xaeros-minimap/files/2705897/download
 
+#------------------------------------------------------------------------
+# Thermal Expansion
+
+wget \
+  -O jar/RedstoneFlux-1.12-2.1.0.6-universal.jar \
+  https://www.curseforge.com/minecraft/mc-mods/redstone-flux/download/2623090/file
+
+wget \
+  -O jar/CoFHCore-1.12.2-4.6.2.25-universal.jar \
+  https://www.curseforge.com/minecraft/mc-mods/cofhcore/download/2669253/file
+
+wget \
+  -O jar/CoFHWorld-1.12.2-1.3.0.6-universal.jar \
+  https://www.curseforge.com/minecraft/mc-mods/cofh-world/download/2623091/file
+
+wget \
+  -O jar/ThermalFoundation-1.12.2-2.6.2.26-universal.jar \
+  https://www.curseforge.com/minecraft/mc-mods/thermal-foundation/download/2669258/file
+
+wget \
+  -O jar/CodeChicken-Lib-1.12.2-3.2.2.353-universal.jar \
+  https://www.curseforge.com/minecraft/mc-mods/codechicken-lib-1-8/download/2618630/file
+
+wget \
+  -O jar/ThermalExpansion-1.12.2-5.5.3.41-universal.jar \
+  https://www.curseforge.com/minecraft/mc-mods/thermalexpansion/download/2669257/file
+
+#------------------------------------------------------------------------
+# Verification
+
+openssl sha256 jar/* | tee received.sha256
+diff -u received.sha256 CHECKSUMS.sha256
+rm -f received.sha256
